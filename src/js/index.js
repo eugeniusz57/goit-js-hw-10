e74f283a8f0fc11677e148ca30a4226f880a4629
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2022-09-24 15:30:41 +0200

    creat fun

--- src/js/index.js
+++ src/js/index.js
@@ -11,34 +11,34 @@ refs.input.addEventListener('input', debounce( onInput, DEBOUNCE_DELAY));
 function onInput(e) {
     const value = e.target.value.trim();
 if(value){
-    fetchCountries(value)
-    .then(onCreateList)
-    .catch(onError)
+    fetchCountries(value).then(onCreateList).catch(onError)
 } refs.countryInfo.innerHTML = '';
 refs.countryList.innerHTML = '';
 }
 
 function onCreateList(arreys) {
     if(arreys.length > 1 && arreys.length <= 10  ){
-
-        refs.countryList.innerHTML = arreys.map(arrey => `<li class="country-item">
-        <img src ="${arrey.flags.svg} " width = "20" height ="20">
-        <h2>${arrey.name.official}</h2>
-        </li>`).join('');
+        refs.countryList.innerHTML = createList(arreys);
 
     } else if(arreys.length > 10 ){
         Notiflix.Notify.info(' Too many matches found. Please enter a more specific name.');
     }  else {
-
-        refs.countryList.innerHTML = arreys.map(arrey => `<li class="country-item">
-        <img src ="${arrey.flags.svg} " width = "20" height ="20">
-        <h2>${arrey.name.official}</h2>
-        </li>`).join('');
-        refs.countryInfo.innerHTML = arreys.map(arrey => `<p><b>Capital:</b> ${arrey.capital}</p> <p><b>Population:</b> ${arrey.population}</p> <p><b>Languages: </b>${Object.values(arrey.languages).join(', ')}</p>`);
-
+        refs.countryList.innerHTML = createList(arreys)
+        refs.countryInfo.innerHTML = createInfoCountry(arreys);
     }
 }
 
 function onError(error) {
     Notiflix.Notify.failure(`❌ Oops, there is no country with that name`)
+}
+
+function createList(arreys) {
+    return arreys.map(arrey => `<li class="country-item">
+    <img src ="${arrey.flags.svg} " width = "20" height ="20">
+    <h2>${arrey.name.common}</h2>
+    </li>`).join('');
+}
+
+function createInfoCountry(arreys) {
+    return arreys.map(arrey => `<p><b>Capital:</b> ${arrey.capital}</p> <p><b>Population:</b> ${arrey.population}</p> <p><b>Languages: </b>${Object.values(arrey.languages).join(', ')}</p>`);
 }
